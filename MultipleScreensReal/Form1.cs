4de2e9d592586c7b43a2eba044b074fa051f5c15
Aimen Ansari <umeaansa555@ed.amdsb.ca>
2023-02-07 09:53:13 -0500

    Add project files.

---
+++ MultipleScreensReal/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MultipleScreensReal
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            MenuScreen ms = new MenuScreen();
+
+            this.Controls.Add(ms);
+
+            // Centre the screen on the Form  
+            ms.Location = new Point((this.ClientSize.Width - ms.Width) / 2,
+            (this.ClientSize.Height - ms.Height) / 2);
+
+
+        }
+    }
+}
---
+++ MultipleScreensReal/MenuScreen.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace MultipleScreensReal
+{
+    partial class MenuScreen
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // openButton
+            // 
+            this.openButton.Location = new System.Drawing.Point(24, 41);
+            this.openButton.Name = "openButton";
+            this.openButton.Size = new System.Drawing.Size(102, 76);
+            this.openButton.TabIndex = 0;
+            this.openButton.Text = "Open";
+            this.openButton.UseVisualStyleBackColor = true;
+            this.openButton.Click += new System.EventHandler(this.openButton_Click);
+            // 
+            // MenuScreen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Highlight;
+            this.Controls.Add(this.openButton);
+            this.Name = "MenuScreen";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button openButton;
+    }
+}
