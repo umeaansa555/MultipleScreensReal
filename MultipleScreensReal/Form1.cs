f9fff0245a51cf8d98880663e2b87d5743151e1f
Aimen Ansari <umeaansa555@ed.amdsb.ca>
2023-02-08 10:05:44 -0500

    single method achived on form 1. still needs to be implemented in other user control screens

--- MultipleScreensReal/Form1.cs
+++ MultipleScreensReal/Form1.cs
@@ -21,16 +21,48 @@ namespace MultipleScreensReal
             InitializeComponent();
 
             //MenuScreen ms = new MenuScreen();
-            LoginScreen ls = new LoginScreen();
 
-            this.Controls.Add(ls);
+
+                //LoginScreen ls = new LoginScreen();
+
+                //this.Controls.Add(ls);
 
             // Centre the screen on the Form  
             //ms.Location = new Point((this.ClientSize.Width - ms.Width) / 2,
             //(this.ClientSize.Height - ms.Height) / 2);
-            ls.Location = new Point((this.ClientSize.Width - ls.Width) / 2,
-            (this.ClientSize.Height - ls.Height) / 2);
 
+
+                //ls.Location = new Point((this.ClientSize.Width - ls.Width) / 2,
+                //(this.ClientSize.Height - ls.Height) / 2);
+
+               ChangeScreen(this, new LoginScreen());
+
+
+
+        }
+        public static void ChangeScreen(object sender, UserControl next)
+
+        {
+
+            Form f; // will either be the sender or parent of sender 
+            if (sender is Form)
+            {
+                f = (Form)sender;
+            }
+            else
+            {
+                UserControl current = (UserControl)sender;
+
+                f = current.FindForm();
+
+                f.Controls.Remove(current);
+            }
+            next.Location = new Point((f.ClientSize.Width - next.Width) / 2,
+                (f.ClientSize.Height - next.Height) / 2);
+
+            f.Controls.Add(next);
+
+            next.Focus();
 
         }
     }
